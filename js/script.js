fe36addeb03bc89632ebdfa271af780977949de8
=siddarth kumar <=>
2016-12-31 09:59:19 +0530

    my first commit for the js task

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function add(first ,second) 
+	{
+		var result =  first + second;
+		if (result>100)
+		{
+			alert(result);
+		}
+		else
+		{
+			alert("YOUR RESULT IS "+result+" WHICH IS NOT GREATER THAN 100");
+		}
+		
+	}
+	
---
+++ js/test-script.js
@@ -0,0 +1,12 @@
+function scream(name) 
+{
+	for(var i = 0;i<500;i++)
+	{
+		console.log("your name is "+name)
+		if(i==436)
+			continue;
+		console.log("this executes");
+	}
+
+}
+
